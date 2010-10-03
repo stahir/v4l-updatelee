d9f5df9afe62db2c87dc997fbda290806b7e49e8
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-10-03 05:27:59 -0300

    [media] Add support for Kworld SBTVD board
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/saa7134/saa7134-dvb.c
+++ drivers/media/video/saa7134/saa7134-dvb.c
@@ -52,6 +52,7 @@
 #include "tda18271.h"
 #include "lgdt3305.h"
 #include "tda8290.h"
+#include "mb86a20s.h"
 
 #include "zl10353.h"
 
@@ -226,6 +227,20 @@ static struct mt352_config avermedia_xc3028_mt352_dev = {
 	.demod_address   = (0x1e >> 1),
 	.no_tuner        = 1,
 	.demod_init      = mt352_avermedia_xc3028_init,
+};
+
+static struct tda18271_std_map mb86a20s_tda18271_std_map = {
+	.dvbt_6   = { .if_freq = 3300, .agc_mode = 3, .std = 4,
+		      .if_lvl = 7, .rfagc_top = 0x37, },
+};
+
+static struct tda18271_config kworld_tda18271_config = {
+	.std_map = &mb86a20s_tda18271_std_map,
+	.gate    = TDA18271_GATE_DIGITAL,
+};
+
+static const struct mb86a20s_config kworld_mb86a20s_config = {
+	.demod_address = 0x10,
 };
 
 /* ==================================================================
@@ -605,6 +620,28 @@ static struct tda827x_config tda827x_cfg_2_sw42 = {
 	.config = 2,
 	.switch_addr = 0x42
 };
+
+/* ------------------------------------------------------------------ */
+
+static int kworld_sbtvd_i2c_gate_ctrl(struct dvb_frontend *fe, int enable)
+{
+	struct tda18271_state *state = fe->demodulator_priv;
+	struct i2c_msg msg = {.addr = 0x4b, .flags = 0, .len = 2};
+	unsigned char msg_enable[2] = { 0x45, 0xc1 };
+	unsigned char msg_disable[2] = { 0x45, 0x81 };
+
+	if (enable)
+		msg.buf = msg_enable;
+	else
+		msg.buf = msg_disable;
+	if (i2c_transfer(state->i2c, &msg, 1) != 1) {
+		struct saa7134_dev *dev = fe->dvb->priv;
+		wprintk("could not access the I2C gate\n");
+		return -EIO;
+	}
+	msleep(20);
+	return 0;
+}
 
 /* ------------------------------------------------------------------ */
 
@@ -1612,6 +1649,19 @@ static int dvb_init(struct saa7134_dev *dev)
 				   0x60, &dev->i2c_adap,
 				   &dtv1000s_tda18271_config);
 		}
+		break;
+	case SAA7134_BOARD_KWORLD_PCI_SBTVD_FULLSEG:
+		fe0->ops.i2c_gate_ctrl =
+		fe0->dvb.frontend = dvb_attach(mb86a20s_attach,
+					       &kworld_mb86a20s_config,
+					       &dev->i2c_adap);
+		fe0->ops.i2c_gate_ctrl(fe, 1);
+		if (fe0->dvb.frontend != NULL) {
+			dvb_attach(tda18271_attach, fe0->dvb.frontend,
+				   0x60, &dev->i2c_adap,
+				   &kworld_tda18271_config);
+		}
+
 		break;
 	default:
 		wprintk("Huh? unknown DVB card?\n");
